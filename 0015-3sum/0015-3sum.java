0018d9e7d5f4234e7572fd059f8c42a48725f21b
V Lohith <135720439+Lohith2005@users.noreply.github.com>
2025-06-28 08:18:18 +0530

    Time: 29 ms (73.98%), Space: 51.3 MB (92.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,20 +4,20 @@ class Solution {
         List<List<Integer>> harsh=new ArrayList<>();
         int n=nums.length;
         for(int i=0;i<n;i++){
-            if(i > 0 && nums[i] == nums[i-1])
+            if(i>0 && nums[i]==nums[i-1])
                 continue;
             int j=i+1;
             int k=n-1;
             while(j<k){
                 int total=nums[i]+nums[j]+nums[k];
-                if(total < 0)
+                if(total<0)
                     j++;
-                else if( total > 0)
+                else if(total>0)
                     k--;
                 else{
                     harsh.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
-                    while( j<k && nums[j] == nums[j-1])
+                    while(j<k && nums[j]==nums[j-1])
                         j++;
                 }
             }
